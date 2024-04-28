f0269cac37bf3aa43f7abc292029aff5e0d3669f
Steve Williams <steve@stevewilliams.me.uk>
2024-04-28 17:47:35 +0100

    Useless lat/lng text fields

--- src/app.jsx
+++ src/app.jsx
@@ -8,14 +8,20 @@ export function App() {
   const [lng, setLng] = useState(13.41)
   const [weatherData, setWeatherData] = useState()
 
-  useEffect(async function() {
+  async function loadWeather() {
     const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`)
     const weatherJson = await response.json()
     setWeatherData({
       temperature: weatherJson.current.temperature_2m,
       temperatureUnit: weatherJson.current_units.temperature_2m
     })
+  }
+
+  useEffect(async function() {
+    await loadWeather();
   }, []);
+
+  console.log(`Lat ${lat} Lng ${lng}`)
 
   return (
     <>
@@ -28,6 +34,14 @@ export function App() {
         </a>
       </div>
       <h1>Vite + Preact Weather Demo App</h1>
+      <div class="card">
+        <h2>Location</h2>
+        <form>
+          Lat: <input type="text" value={lat} />
+          <br />
+          Lng: <input type="text" value={lng} />
+        </form>
+      </div>
       { weatherData && (
         <div class="card">
           <p>Current temperature is { weatherData.temperature }{ weatherData.temperatureUnit }</p>
